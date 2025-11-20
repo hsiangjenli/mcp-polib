0f99e661de53792356971e4f06f1895b793ad7f9
Hsiang-Jen Li <hsiangjenli@gmail.com>
2025-11-20 17:28:07 +0800

    fix: Docker image build process by combining build and tag commands

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -4,6 +4,4 @@ USERNAME="hsiangjenli"
 IMAGE_NAME="$USERNAME/mcp-polib"
 date_tag=$(date +%Y-%m-%d)
 
-docker build --no-cache -t $IMAGE_NAME:$date_tag --push .
-docker tag $IMAGE_NAME:$date_tag $IMAGE_NAME:latest
-docker push $IMAGE_NAME:latest
+docker build --no-cache -t $IMAGE_NAME:$date_tag -t $IMAGE_NAME:latest --push .
